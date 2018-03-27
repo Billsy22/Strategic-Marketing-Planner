fe7521d2a55b0150f5ca6461f7e57c30320c7a77
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-03-27 13:57:38 -0600

    Created a DateHelper.swift file to convert string to date. Made a save button tapped action in AddClientModalViewController.

--- Strategic Marketing Planner/ViewControllers/AddClientModalViewController.swift
+++ Strategic Marketing Planner/ViewControllers/AddClientModalViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class AddClientModalViewController: UIViewController {
-
+    
     // MARK: -  Properites
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -34,20 +34,28 @@ class AddClientModalViewController: UIViewController {
     
     // MARK: -  Update Views
     func updateViews() {
-        guard let client = client else { print("No Client Found \(#file)\(#function)"); return }
-        firstNameTextField.text = client.firstName
-        lastNameTextField.text = client.lastName
-        // TODO: -  Add photo property for button
-        practiceNameTextField.text = client.practiceName
-        phoneTextField.text = client.phoneNumber
-        emailTextField.text = client.email
-        addressTextField.text = client.streetAddress
-        cityTextField.text = client.city
-        stateTextField.text = client.state
-        zipCodeTextField.text = client.zip
-        guard let contactDate = client.contactDate else { print("No contact date for client"); return }
-        initialContactDateTextField.text = "\(contactDate)"
-        notesTextView.text = client.notes
+        if let client = client {
+            firstNameTextField.text = client.firstName
+            lastNameTextField.text = client.lastName
+            // TODO: -  Add photo property for button
+            practiceNameTextField.text = client.practiceName
+            phoneTextField.text = client.phoneNumber
+            emailTextField.text = client.email
+            addressTextField.text = client.streetAddress
+            cityTextField.text = client.city
+            stateTextField.text = client.state
+            zipCodeTextField.text = client.zip
+            guard let contactDate = client.contactDate else { print("No contact date for client"); return }
+            initialContactDateTextField.text = "\(contactDate)"
+            notesTextView.text = client.notes
+            saveOrRemoveClientButton.setTitle("Delete Client", for: .normal)
+            saveOrRemoveClientButton.backgroundColor = .red
+        } else {
+            print("No Client Found \(#file)\(#function)")
+            saveOrRemoveClientButton.setTitle("Save Client", for: .normal)
+            saveOrRemoveClientButton.backgroundColor = .brandBlue
+            return
+        }
     }
     
     // MARK: -  Actions
@@ -57,14 +65,41 @@ class AddClientModalViewController: UIViewController {
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let firstName = firstNameTextField.text,
+        let lastName = lastNameTextField.text,
+        let practiceName = practiceNameTextField.text,
+        let phone = phoneTextField.text,
+        let email = emailTextField.text,
+        let streetAddress = addressTextField.text,
+            let zip = zipCodeTextField.text,
+        let city = cityTextField.text,
+        let state = stateTextField.text,
+        let initialContactDateString = initialContactDateTextField.text,
+            let notes = notesTextView.text else { return }
+            if firstName.isEmpty || lastName.isEmpty || practiceName.isEmpty || phone.isEmpty || email.isEmpty || streetAddress.isEmpty || streetAddress.isEmpty || zip.isEmpty {
+            let emptyTextAlert = UIAlertController(title: "Required text field empty", message: "Please fill out anything with a *", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+                print("Alert Dismissed")
+            })
+            emptyTextAlert.addAction(okAction)
+            present(emptyTextAlert, animated: true, completion: nil)
+        } else {
+                let initialContactDate = DateHelper.dateFrom(string: initialContactDateString)
+            ClientController.shared.addClient(withFirstName: firstName, lastName: lastName, practiceName: practiceName, phone: phone, email: email, streetAddress: streetAddress, city: city, state: state, zip: zip, initialContactDate: initialContactDate, notes: notes)
+                dismiss(animated: true, completion: {
+                    print("Client Created")
+                })
+        }
     }
-    */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
