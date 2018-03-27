e4dac74aef3707d46d23708cf275f5ac6bb9cc80
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-03-27 10:01:33 -0600

    Finished update views function but keep finding nil when unwrapping client. Need to quit xcode to get rid of some error messages

--- Strategic Marketing Planner/ViewControllers/AddClientModalViewController.swift
+++ Strategic Marketing Planner/ViewControllers/AddClientModalViewController.swift
@@ -11,16 +11,55 @@ import UIKit
 class AddClientModalViewController: UIViewController {
 
     // MARK: -  Properites
-    var client: Client?
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var clientPhotoButton: UIButton!
+    @IBOutlet weak var practiceNameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var initialContactDateTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var saveOrRemoveClientButton: UIButton!
+    var client: Client? {
+        didSet {
+            saveOrRemoveClientButton.titleLabel?.text = "Delete Client"
+        }
+    }
     
     // MARK: -  Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
+    // MARK: -  Update Views
+    func updateViews() {
+        guard let client = client else { print("No Client Found \(#file)\(#function)"); return }
+        firstNameTextField.text = client.firstName
+        lastNameTextField.text = client.lastName
+        // TODO: -  Add photo property for button
+        practiceNameTextField.text = client.practiceName
+        phoneTextField.text = client.phoneNumber
+        emailTextField.text = client.email
+        addressTextField.text = client.streetAddress
+        cityTextField.text = client.city
+        stateTextField.text = client.state
+        zipCodeTextField.text = client.zip
+        guard let contactDate = client.contactDate else { print("No contact date for client"); return }
+        initialContactDateTextField.text = "\(contactDate)"
+        notesTextView.text = client.notes
+    }
     
+    // MARK: -  Actions
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true) {
+            print("View Dismissed")
+        }
+    }
     
 
     /*
