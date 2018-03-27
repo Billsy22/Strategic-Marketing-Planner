577f39874f82576ca8f78616435713dddc1b5a58
Christopher Thiebaut <christopherthiebaut@gmail.com>
2018-03-27 09:48:36 -0600

    Make buttons on PresentationBaseVC functional.  Refactor PresentationBaseVC and NavigationTVC to depend on protocols instead of directly on each other.  Add a field for client on PresentationBaseVC.

--- Strategic Marketing Planner/ViewControllers/PresentationBaseViewController.swift
+++ Strategic Marketing Planner/ViewControllers/PresentationBaseViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class PresentationBaseViewController: UIViewController, NavigationTableViewControllerDelegate {
+protocol PresentationBaseViewControllerNavigationPane: class {
+    var delegate: PresentationBaseViewControllerNavigationPaneDelegate? { get set }
+    
+    var destinations: [String] { get set }
+    
+    func requestMoveToDestination(index: Int)
+}
+
+class PresentationBaseViewController: UIViewController, PresentationBaseViewControllerNavigationPaneDelegate {
+    
+    weak var navigationPane: PresentationBaseViewControllerNavigationPane?
+    
+    var client: Client?
 
     @IBOutlet weak var mainContentView: UIView!
     lazy var destinations: [(destinationName: String, destinationViewController: UIViewController)] = setupDefaultDestinations()
     
+    @IBOutlet var navigationBarPreviousButton: UIBarButtonItem!
+    @IBOutlet weak var navigationBarNextButton: UIBarButtonItem!
+    @IBOutlet weak var navigationBarClientButton: UIBarButtonItem!
+    
     @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +40,26 @@ class PresentationBaseViewController: UIViewController, NavigationTableViewContr
         navigationBar.barTintColor = UIColor.brandBlue
     }
     
-    // MARK: - Navigation
+    // MARK: - Configure Embedded VCs
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         if segue.identifier == "toEmbeddedNavigationVC" {
-            guard let navigationTVC = segue.destination as? NavigationTableViewController else {return}
+            guard let navigationTVC = segue.destination as? PresentationBaseViewControllerNavigationPane else {return}
             navigationTVC.delegate = self
             var destinationNames: [String] = []
             for destination in destinations {
                 destinationNames.append(destination.destinationName)
             }
             navigationTVC.destinations = destinationNames
+            navigationPane = navigationTVC
         }
     }
     
     func selectedDestinationAtIndex(_ index: Int) {
+        currentIndex = index
         let destination = destinations[index].destinationViewController
         addChildViewController(destination)
         for subview in mainContentView.subviews {
@@ -48,6 +68,16 @@ class PresentationBaseViewController: UIViewController, NavigationTableViewContr
         mainContentView.addSubview(destination.view)
         destination.view.frame = mainContentView.bounds
         destination.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
+    private func setupNavBarAppearance(){
+        let attrs = [
+            NSAttributedStringKey.foregroundColor: UIColor.red,
+            NSAttributedStringKey.font: UIFont(name: "Georgia-Bold", size: 24)!
+        ]
+        
+        /*UINavigationBar.appearance()*/
+        navigationBarPreviousButton.setTitleTextAttributes(attrs, for: .normal)
     }
     
     private func setupDefaultDestinations() ->  [(String, UIViewController)]{
@@ -63,5 +93,18 @@ class PresentationBaseViewController: UIViewController, NavigationTableViewContr
         defaultDestinations.append(("Green", greenVC))
         return defaultDestinations
     }
-
+    
+    @IBAction func previousButtonTapped(_ sender: Any) {
+        if currentIndex - 1 >= 0 {
+            navigationPane?.requestMoveToDestination(index: currentIndex - 1)
+        }
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        if currentIndex + 1 < destinations.count {
+            navigationPane?.requestMoveToDestination(index: currentIndex + 1)
+        }
+    }
+    
+    
 }
