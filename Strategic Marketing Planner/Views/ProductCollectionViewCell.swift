cb5da6d411d073c534d44b5a3427617825907a0c
stevenbrown33 <stevebrown33@me.com>
2018-03-29 11:56:13 -0600

    collection view, added pdfs

---
+++ Strategic Marketing Planner/Views/ProductCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCollectionViewCell.swift
+//  Strategic Marketing Planner
+//
+//  Created by Steven Brown on 3/28/18.
+//  Copyright © 2018 Christopher Thiebaut. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    
+}
