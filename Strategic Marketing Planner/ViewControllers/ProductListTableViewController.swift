4f9a61fbc3532cf50c610a7ceb18c7f992eee45e
Cruiz <cruiz@github.com>
2018-04-02 12:01:45 -0600

    Updated Product List storyboard, product list TVC, and product detail vc.

--- Strategic Marketing Planner/ViewControllers/ProductListTableViewController.swift
+++ Strategic Marketing Planner/ViewControllers/ProductListTableViewController.swift
@@ -13,43 +13,34 @@ class ProductListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return ProductController.shared.products.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
+        
+        // you want one product for every cell - get the indexPath.row out of yoour source of truth
 
-
+        let product = ProductController.shared.products[indexPath.row]
+        cell.textLabel?.text = product.title
+        
         return cell
-    
     }
-
-    
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-        }
-    }
-    
-
     
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        if segue.identifier == "toProductDetail" {
+            guard let destinationVC = segue.destination as? ProductDetailViewController,
+                let selectedProduct = tableView.indexPathForSelectedRow?.row else { return }
+                let product = ProductController.shared.products[selectedProduct]
+            destinationVC.product = product
+            
+        }
     }
-    
-
 }
