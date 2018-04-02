6a079d3f6a3d181c38af309ae54600558fc4f4e7
stevenbrown33 <stevebrown33@me.com>
2018-04-02 15:11:22 -0600

    added colors to branding helper file. created growth calculator helper file.

--- Strategic Marketing Planner/Resources/Branding.swift
+++ Strategic Marketing Planner/Resources/Branding.swift
@@ -16,4 +16,16 @@ extension UIColor {
     public static var brandBlue: UIColor {
         return #colorLiteral(red: 0.1180489585, green: 0.4012622237, blue: 0.612983048, alpha: 1)
     }
+    
+    public static var returnGreen: UIColor {
+        return UIColor(red: 118.0/255.0, green: 157.0/255.0, blue: 47.0/255.0, alpha: 1.0)
+    }
+    
+    public static var goalBlue: UIColor {
+        return UIColor(red: 15.0/255.0, green: 68.0/255.0, blue: 112.0/255.0, alpha: 1.0)
+    }
+    
+    public static var currentBlue: UIColor {
+        return UIColor(red: 25.0/255.0, green: 108.0/255.0, blue: 176.0/255.0, alpha: 1.0)
+    }
 }
