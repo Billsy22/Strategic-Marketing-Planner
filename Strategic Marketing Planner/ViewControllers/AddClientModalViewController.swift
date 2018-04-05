e05ba7e1b9e60bb0b1ae07ee9e0de795efa634eb
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-04-05 12:09:38 -0600

    Fixed issue with worng client being set when new clien is created

--- Strategic Marketing Planner/ViewControllers/AddClientModalViewController.swift
+++ Strategic Marketing Planner/ViewControllers/AddClientModalViewController.swift
@@ -20,6 +20,7 @@ class AddClientModalViewController: UIViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var clientPhotoButton: UIButton!
     @IBOutlet weak var practiceNameTextField: UITextField!
+    @IBOutlet weak var practiceTypeDropDownButton: PracticeTypeDropDownButton!
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
@@ -48,7 +49,9 @@ class AddClientModalViewController: UIViewController {
         cityTextField.delegate = self
         stateTextField.delegate = self
         zipCodeTextField.delegate = self
+        
         imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
@@ -156,8 +159,7 @@ extension AddClientModalViewController {
             if let client = client {
                 ClientController.shared.updateClient(client, withFirstName: firstName, lastName: lastName, practiceName: practiceName, phone: phone, email: email, streetAddress: streetAddress, city: city, state: state, zip: zip, notes: notes)
             } else {
-                ClientController.shared.addClient(withFirstName: firstName, lastName: lastName, practiceName: practiceName, phone: phone, email: email, streetAddress: streetAddress, city: city, state: state, zip: zip, initialContactDate: Date(), notes: notes)
-                self.client = ClientController.shared.clients.last
+                self.client = ClientController.shared.addClient(withFirstName: firstName, lastName: lastName, practiceName: practiceName, phone: phone, email: email, streetAddress: streetAddress, city: city, state: state, zip: zip, initialContactDate: Date(), notes: notes)
             }
             dismiss(animated: true, completion: {
                 print("Client Created")
--- Strategic Marketing Planner/Views/PracticeTypeDropDownButton.swift
+++ Strategic Marketing Planner/Views/PracticeTypeDropDownButton.swift
@@ -10,13 +10,92 @@ import UIKit
 
 class PracticeTypeDropDownButton: UIButton {
     
+    // MARK: -  Properties
+    var dropDownView = DropDownView()
+    var dropDownHeight = NSLayoutConstraint()
+    var isOpen = false
+    
     // MARK: -  Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        dropDownView = DropDownView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: -  Life Cycles
+    override func didMoveToSuperview() {
+        dropDownView.translatesAutoresizingMaskIntoConstraints = false
+        self.superview?.addSubview(dropDownView)
+        self.superview?.bringSubview(toFront: dropDownView)
+        dropDownView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        dropDownView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        dropDownView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        dropDownHeight = dropDownView.heightAnchor.constraint(equalToConstant: 0)
+    }
+    
+    // MARK: -  Event methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isOpen == false {
+            isOpen = true
+            NSLayoutConstraint.deactivate([self.dropDownHeight])
+            self.dropDownHeight.constant = 150
+            NSLayoutConstraint.activate([self.dropDownHeight])
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                self.dropDownView.layoutIfNeeded()
+                self.dropDownView.center.y += self.dropDownView.frame.height / 2            }, completion: nil)
+        } else {
+            isOpen = false
+            NSLayoutConstraint.deactivate([self.dropDownHeight])
+            self.dropDownHeight.constant = 0
+            NSLayoutConstraint.activate([self.dropDownHeight])
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                self.dropDownView.center.y -= self.dropDownView.frame.height / 2
+                self.dropDownView.layoutIfNeeded()
+            }, completion: nil)
+        }
+    }
+}
+
+class DropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: -  Instance Properties
+    var dropDownOptions: [String] = []
+    var tableView = UITableView()
+    
+    // MARK: -  Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(tableView)
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: -  Table View Data Source Functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Client.practiceTypes.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(Client.practiceTypes[indexPath.row])".capitalized
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
 }
